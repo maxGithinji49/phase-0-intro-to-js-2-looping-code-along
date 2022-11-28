0e97fc729c9c302025a52647b732e373f1f4c09c
Max <maxwell.githinji@student.moringaschool.com>
2022-11-28 16:16:05 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names= ['Guadalupe', 'Olie', 'Aki']
+function writeCards(names){
+    const messages = []
+     
+    for(let i = 0; i < names.length; i++){
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        messages.push(message);
+    }
+    return messages
+ }
+
+ function countDown (num) {
+    while (num>=0)
+   {
+       console.log(num--)
+   }
+}
